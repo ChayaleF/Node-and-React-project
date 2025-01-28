00e79a6bf5447d189c71dffe0a11331c28b13e90
chayale <c0556728176@gmail.com>
2025-01-28 13:33:41 +0200

    update user

--- server/app.ts
+++ server/app.ts
@@ -1,18 +1,56 @@
 import express from 'express';
 import dotenv from 'dotenv'
-import sequelize from './dataAccess/dataAccess'
-import { Users } from './models/users';
-//import setupSwagger from './swaggerConfig';
+import { User } from './models/user';
+import setupSwagger from './swaggerConfig';
 import usersController from './controllers/usersController';
+import businessController from './controllers/businessController';
+import servicesController from './controllers/serviceController'
 dotenv.config()
 const port=process.env.PORT||8000
 const app= express();
 app.use(express.json());
-
-//setupSwagger(app);
+// sequelize.sync({ force: true }).then(() => {
+//   console.log('Database & tables created!');
+// });
+setupSwagger(app);
 app.use('/users', usersController);
+app.use('/business', businessController);
+app.use('/services', servicesController);
 
 app.listen(port, async () => {
     console.log(`Server is running at http://localhost:${port}/docs`);
 
 });
+
+
+
+
+// import express from 'express';
+// import dotenv from 'dotenv'
+// import sequelize from './dataAccess/dataAccess'
+// import { User } from './models/user';
+// import setupSwagger from './swaggerConfig';
+// import usersController from './controllers/usersController';
+// dotenv.config()
+// const port=process.env.PORT||8000
+// const app= express();
+// // sequelize.sync({ force: true }).then(() => {
+// //   console.log('Database & tables created!');
+// // });
+// app.use(express.json());
+
+// setupSwagger(app);
+// app.use('/users', usersController);
+
+// app.listen(port, async () => {
+//     console.log(`Server is running at http://localhost:${port}/docs`);
+
+// });
+// sequelize.authenticate().then(async () => {
+//   await Users.sync();
+//   const newUser = await Users.create({
+//       userName: "aa",
+//       userPassword:"12345",
+//       userToken: "ddd"
+//   });
+// });
--- server/controllers/usersController.ts
+++ server/controllers/usersController.ts
@@ -1,26 +1,35 @@
-import { Users } from "../models/users";
+import { User } from "../models/user";
 import { Request, Response } from 'express';
 
 import express from 'express';
+import { addUser, login } from "../services/usersService";
+import { CustomError } from "../errors/CustomError";
 const router = express.Router();
-router.get('/', async (req: Request, res: Response) => {
-    try {
-        const users = await Users.findOne({
-            where: {
-                id: 2
-            },
-            raw: true
-        })
-        
-        if (!users) {
-            return res.status(404).json({ message: 'User not found' });
-        }
 
-        res.status(200).json(users);
-    } catch (err) {
-       
-            res.status(500).json({ message: 'Internal server error' });
-       
+router.post('/signUp', async (req: Request, res: Response) => {
+    try {
+        const result = await addUser(req.body);
+        res.status(201).json(result);
+    } catch (err: any) {
+        if (err instanceof CustomError) {
+            res.status(err.statusCode).send(err.message);
+        } else {
+            res.status(500).send('Internal Server Error');
+        }
+        console.error('Error:', err);
+    }
+});
+router.post('/login', async (req: Request, res: Response) => {
+    try {
+        const { loginEmail, loginPassword } = req.body;
+        const result = await login(loginEmail, loginPassword);
+        res.status(200).json(result);
+    } catch (err: any) {
+        if (err instanceof CustomError) {
+            res.status(err.statusCode).send(err.message);
+        } else {
+            res.status(500).send('Internal Server Error');
+        }
         console.error('Error:', err);
     }
 });
